cc273be1638426c95634356f1df6d4806857754b
yejunbaek <104457875+yejunbaek@users.noreply.github.com>
2022-04-26 14:38:11 -0400

    Add files via upload

--- strings3.py
+++ strings3.py
@@ -1,8 +1,11 @@
 
-first = input("Enter a word: ")
-length = len(first)
-print(length)
-while length > 2:
-    first = input("Enter a word: ")
-    length = len(first)
-    print(length)
+userinput = input("Enter a word: ")
+while userinput[0] != userinput[1]  or len(userinput) > 2:
+    userinput = input("Enter a word: ")
+userinput2 = input("Enter a second word: ")
+while userinput != "**" and userinput2 != "**":
+    length = len(userinput)//2
+    print(userinput[:length],"_",userinput2,"_",userinput[length:])
+    userinput = input("Enter a word: ")
+    userinput2 = input("Enter a second word: ")
+print("Goodbye")
